f58592dff00d949f83b8868efa2249ce937427af
lmadrigal-ux <lmadrigal@vacunar.com.ar>
2025-10-01 12:58:16 -0300

    se agrega healthCheck controller

---
+++ Chat.Api/Controllers/RootController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Chat.Api.Controllers
+{
+    [ApiController]
+    [Route("/")]
+    public class RootController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("CONNECTED");
+        }
+    }
+}
